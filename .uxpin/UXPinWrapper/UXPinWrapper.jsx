40a3f078c95e5bd3fd2ef5f61ae131a7633f1080
서솔민 <solgo123@gmail.com>
2022-11-07 11:42:32 +0900

    :bug: fix tsconfig / ts-loader to run uxpin

--- .uxpin/UXPinWrapper/UXPinWrapper.jsx
+++ .uxpin/UXPinWrapper/UXPinWrapper.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-
+import theme from '../../src/common/theme/index';
 export default function UXPinWrapper({ children }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
---
+++ src/Button/presets/0-default.jsx
@@ -0,0 +1,7 @@
+import Button from '../Button';
+
+export default (
+  <Button type="button" variant="default" size="lg" disabled={false}>
+    Preset
+  </Button>
+);
