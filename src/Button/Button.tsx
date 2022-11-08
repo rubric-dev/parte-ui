51d549dfa703ddb8026df6609e17a28ffc39a56e
서솔민 <solgo123@gmail.com>
2022-11-08 17:22:27 +0900

    test_3

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -20,7 +20,7 @@ const Button = ({ children, ...props }: ButtonProps) => {
   return (
     <StyledButton
       type={props.type}
-      onClick={() => console.log('test1')}
+      onClick={() => console.log('test121')}
       disabled={props.disabled}
       fullWidth={props.fullWidth}
       size={props.size}
