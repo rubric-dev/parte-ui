149e573ce7ada39353bd7e3d96cb4fc896986d7e
june6723 <june6723@naver.com>
2022-11-16 17:42:05 +0900

    add/ActionIcons

---
+++ src/Icons/daekyun/ActionCircleIcon.tsx
@@ -0,0 +1,20 @@
+function ActionCircleIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8 0C3.6 0 0 3.6 0 8C0 12.4 3.6 16 8 16C12.4 16 16 12.4 16 8C16 3.6 12.4 0 8 0V0ZM8 14C4.7 14 2 11.3 2 8C2 4.7 4.7 2 8 2C11.3 2 14 4.7 14 8C14 11.3 11.3 14 8 14V14Z"
+        fill="#8F95B2"
+      />
+    </svg>
+  );
+}
+export default ActionCircleIcon;
---
+++ src/Icons/daekyun/ActionSmallPlusIcon.tsx
@@ -0,0 +1,20 @@
+function ActionSmallPlusIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M11 7H9V5C9 4.45 8.55 4 8 4C7.45 4 7 4.45 7 5V7H5C4.45 7 4 7.45 4 8C4 8.55 4.45 9 5 9H7V11C7 11.55 7.45 12 8 12C8.55 12 9 11.55 9 11V9H11C11.55 9 12 8.55 12 8C12 7.45 11.55 7 11 7Z"
+        fill="#8F95B2"
+      />
+    </svg>
+  );
+}
+export default ActionSmallPlusIcon;
