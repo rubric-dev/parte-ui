bff91518acb199b00f8cdf791f783bb4e80868a5
서솔민 <solgo123@gmail.com>
2022-12-02 17:14:00 +0900

    :sparkles: add steps component

---
+++ src/Steps/Steps.stories.tsx
@@ -0,0 +1,44 @@
+import Steps from './Steps';
+import { Story, Meta } from '@storybook/react';
+import { StepsProps } from './Steps.types';
+
+export default {
+  title: 'Components/Steps',
+  component: Steps,
+  parameters: {
+    layout: 'centered',
+    viewport: 'responsive',
+  },
+} as Meta;
+
+const Template: Story<StepsProps> = ({ ...args }) => {
+  return <Steps {...args} />;
+};
+
+const MOCK = [
+  {
+    name: '냉장고 확인',
+  },
+  {
+    name: '운전해서 상점으로 가기',
+  },
+  {
+    name: '필요한 것 구입하기',
+  },
+  {
+    name: '운전해서 집으로 오기',
+  },
+  {
+    name: '음식을 냉장고에 보관하기',
+  },
+].map((v, i) => {
+  return {
+    id: i,
+    label: v.name,
+  };
+});
+export const Default = Template.bind({});
+Default.args = {
+  stepList: MOCK,
+  currentStep: MOCK.findIndex((v) => v.id === 3),
+};
