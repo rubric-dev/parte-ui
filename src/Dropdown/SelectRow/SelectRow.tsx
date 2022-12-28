cfc2bbcd7b88a8c6af029e30b462b1276f953144
solmin <solgo123@gmail.com>
2022-12-28 18:51:35 +0900

    feat: ENter key add

--- src/Dropdown/SelectRow/SelectRow.tsx
+++ src/Dropdown/SelectRow/SelectRow.tsx
@@ -55,10 +55,3 @@ const SelectRow = forwardRef<HTMLDivElement, SelectRowProps>((props, ref) => {
 });
 
 export default SelectRow;
-
-/** 구현 시 주의해야할점
-1. 키보드 이벤트 (화살표 / 검색시) tabIndex
-2. 타이틀도 옵션임 (groupedSelect)
-3. search도 옵션임 (isSearchable)
-4. TagInput이랑 연계해야함
-*/
