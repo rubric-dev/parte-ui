27f5541ae49e60b2fad8914fb98c49f0738ebc54
dohye1 <tsc03018@gmail.com>
2023-02-13 13:52:36 +0900

    :sparkles: Pagination Component

---
+++ src/Pagination/Pagination.stories.tsx
@@ -0,0 +1,44 @@
+import Pagination from './Pagination';
+import { Story, Meta } from '@storybook/react';
+import { PaginationProps } from './Pagination.types';
+import { useCallback, useState } from 'react';
+
+export default {
+  title: 'Components/Controls/Pagination',
+  component: Pagination,
+  parameters: {
+    layout: 'centered',
+    viewport: 'responsive',
+  },
+} as Meta;
+
+const Template: Story<PaginationProps> = ({
+  page: initialPage,
+  totalPages,
+}) => {
+  const [page, setPage] = useState(initialPage);
+
+  const onPageChange = useCallback((index: number) => {
+    setPage(index);
+  }, []);
+
+  return (
+    <Pagination
+      page={page}
+      totalPages={totalPages}
+      onPageChange={onPageChange}
+    />
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  page: 4,
+  totalPages: 10,
+};
+
+export const Under7Page = Template.bind({});
+Under7Page.args = {
+  page: 3,
+  totalPages: 6,
+};
---
+++ src/Pagination/Pagination.tsx
@@ -0,0 +1,142 @@
+import { useCallback } from 'react';
+import * as Styled from './Pagination.styled';
+import { PaginationProps } from './Pagination.types';
+import InterfaceChevronLeftIcon from '../parte-icons/Icons/InterfaceChevronLeftIcon';
+import InterfaceChevronRightIcon from '../parte-icons/Icons/InterfaceChevronRightIcon';
+import { Box } from '../Layout';
+import { Heading } from '../@foundations/Typography';
+import { IconButton } from '../IconButton';
+
+const MAX_HANDLES_TO_SHOW = 7;
+
+type Props = {
+  page: number | string;
+  isSelected: boolean;
+  disabled?: boolean;
+  onPageChange: (page: number) => void;
+  children: React.ReactNode;
+};
+
+const PaginationButton = ({
+  isSelected,
+  onPageChange,
+  disabled = false,
+  page,
+  children,
+}: Props) => {
+  const isEllipsis = typeof page === 'string' && page === '...';
+
+  const onClick = useCallback(() => {
+    if (typeof page === 'number') {
+      onPageChange(page);
+    }
+  }, [page, onPageChange]);
+
+  if (isEllipsis) {
+    return (
+      <Styled.EllipsisText alignItems="Center" justifyContent="Center">
+        <Heading size={300} color="N700">
+          {page}
+        </Heading>
+      </Styled.EllipsisText>
+    );
+  }
+
+  return (
+    <Styled.PaginationButton
+      disabled={disabled}
+      isSelected={isSelected}
+      aria-current={isSelected}
+      aria-label={`Page ${page}`}
+      onClick={onClick}
+    >
+      {children}
+    </Styled.PaginationButton>
+  );
+};
+
+const range = (start: number, stop: number) => {
+  const output = [];
+  for (let i = start; i <= stop; i++) {
+    output.push(i);
+  }
+
+  return output;
+};
+
+const getPaginationButtonContent = ({
+  page,
+  totalPages,
+}: {
+  page: number;
+  totalPages: number;
+}) => {
+  if (totalPages <= MAX_HANDLES_TO_SHOW) {
+    return range(1, totalPages);
+  }
+
+  if (totalPages > MAX_HANDLES_TO_SHOW && page <= 4) {
+    return [...range(1, 5), '...', totalPages];
+  }
+
+  if (totalPages - page < 4) {
+    return [1, '...', ...range(totalPages - 4, totalPages)];
+  }
+
+  return [1, '...', ...range(page - 1, page + 1), '...', totalPages];
+};
+
+const Pagination = ({ page, totalPages, onPageChange }: PaginationProps) => {
+  return (
+    <Box alignItems="Center" padding={0} gap={4}>
+      <IconButton
+        variant="minimal"
+        Icon={
+          <InterfaceChevronLeftIcon
+            size={12}
+            color={page === 1 ? 'disabled' : 'muted'}
+          />
+        }
+        size={32}
+        disabled={page === 1}
+        onClick={() => {
+          if (page > 1) {
+            onPageChange(page - 1);
+          }
+        }}
+      />
+      {getPaginationButtonContent({ totalPages, page }).map((val, i) => {
+        const isSelected = val === page;
+
+        return (
+          <PaginationButton
+            key={i}
+            isSelected={isSelected}
+            page={val}
+            onPageChange={onPageChange}
+          >
+            {val}
+          </PaginationButton>
+        );
+      })}
+      <IconButton
+        variant="minimal"
+        Icon={
+          <InterfaceChevronRightIcon
+            size={12}
+            color={page === totalPages ? 'disabled' : 'muted'}
+          />
+        }
+        size={32}
+        disabled={page === totalPages}
+        onClick={() => {
+          if (page < totalPages) {
+            onPageChange(page + 1);
+          }
+        }}
+      />
+    </Box>
+  );
+};
+
+export default Pagination;
